e3ad86f8e9054332b2420a6f0770fee07290f295
Stephanie Zhu <slzzhu@gmail.com>
2020-10-12 23:32:28 -0700

    Commit with Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <p></p>
+    </Layout>
+  )
+}
+
+export default About;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Hi, I'm Stephanie.</h1>
+      <p>This site was made with Gatsby.</p>
+    </Layout>
+  )
+}
+
+
+export default Home;
